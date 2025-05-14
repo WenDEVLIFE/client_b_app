78e3c958bce3a5d0e80f70102a36841aa271b255
Cyzo50218 <155029018+Cyzo50218@users.noreply.github.com>
2025-05-13 22:15:24 -0800

    Add files via upload

--- app/src/main/java/com/noveleta/sabongbetting/Dashboard/dashboardMain.kt
+++ app/src/main/java/com/noveleta/sabongbetting/Dashboard/dashboardMain.kt
@@ -142,10 +142,9 @@ if(betResponse != null){
             PayoutReceiptDialog(betResponse!!){
             viewModelPayoutData.clearBetState()
             }
-            LaunchedEffect(betResponse) {
-            delay(3000) // 3 seconds
+            
              printPayout(context, betResponse!!)
-            }
+            
             }else if (betResponse == null && betResult == -1) {
             PrintBetPayoutErrorResults(betResult){
             viewModelPayoutData.clearBetState()
