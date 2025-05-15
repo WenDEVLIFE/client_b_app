8df841a20c01b8de2fa20ac2518babac25376300
Cyzo50218 <myfavoritemappingswar@gmail.com>
2025-05-15 20:32:26 +0800

    done

--- app/src/main/java/com/noveleta/sabongbetting/Helper/ScannerOverlay.kt
+++ app/src/main/java/com/noveleta/sabongbetting/Helper/ScannerOverlay.kt
@@ -1,19 +1,23 @@
 package com.noveleta.sabongbetting.Helper
 
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
+
+
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
-import androidx.compose.ui.graphics.drawscope.drawLine
-import androidx.compose.ui.graphics.drawscope.drawRect
+import androidx.compose.ui.graphics.drawscope.DrawScope  // only if you need its type directly
+import androidx.compose.ui.draw.drawWithContent
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.graphics.drawscope.drawWithContent
+import androidx.compose.ui.platform.LocalDensity
+
 
 @Composable
 fun ScannerOverlayBox(
