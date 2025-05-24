30ab30f2ebbfd351124b95fb416b7f082f551000
Cyzo50218 <myfavoritemappingswar@gmail.com>
2025-05-24 23:43:16 +0800

    Done

---
+++ app/src/main/java/com/noveleta/sabongbetting/Model/MoneyOnHand.kt
@@ -0,0 +1,24 @@
+package com.noveleta.sabongbetting.Model;
+
+data class SummaryReport(
+    val errorCode: Int,
+    val reportTitle: String,
+    val systemName: String,
+    val dateTime: String,
+    val username: String,
+    val cashIn: String,
+    val totalBets: String,
+    val totalMobileDeposit: String,
+    val cashOut: String,
+    val totalPayoutPaid: String,
+    val totalCancelledPaid: String,
+    val totalCancelledBet: String,
+    val totalDrawPaid: String,
+    val totalDrawBets: String,
+    val totalDrawBetsPaid: String,
+    val totalMobileWithdraw: String,
+    val moneyOnHand: String,
+    val commisionBody: String,
+    val totalPayoutUnclaimed: String,
+    val totalDrawUnclaimed: String
+)
