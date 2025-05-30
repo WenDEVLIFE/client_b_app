3d98cce3a192a06c4da3a2e04dc18dd645104598
Cyzo50218 <myfavoritemappingswar@gmail.com>
2025-05-30 15:48:42 +0800

    Done

--- app/src/main/java/com/noveleta/sabongbetting/Enter/login.kt
+++ app/src/main/java/com/noveleta/sabongbetting/Enter/login.kt
@@ -26,7 +26,8 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifierimport androidx.compose.ui.draw.clip
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.*
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
